60957f58c13e063fb2f873cf8b6e33fe9c7f5d50
Mahla <107788139+MahlaParvaz@users.noreply.github.com>
2024-03-26 22:29:18 +0330

    feat: presist user in App

--- src/app/Header.tsx
+++ src/app/Header.tsx
@@ -1,21 +1,44 @@
 'use client';
 
 import Link from 'next/link';
+import { useGetUser } from '@/src/hooks/useAuth';
+
+interface User {
+  name: string;
+}
+
+interface Cart {
+  payDetail: {
+    productIds: string[];
+  };
+}
 
 function Header() {
+  const { data, error, isPending } = useGetUser();
+  const { user, cart }: { user?: User; cart?: Cart } = data || {};
+
   return (
-    <header className="shadow-sm  mb-10 sticky top-0 transition-all duration-200 bg-white">
+    <header
+      className={`shadow-sm mb-10 sticky top-0 transition-all duration-200 bg-white ${
+        isPending ? 'blur-sm opacity-70' : 'opacity-100 blur-0'
+      }`}
+    >
       <nav className="flex container xl:max-w-screen-xl">
         <div className="flex-1">
           <ul className="flex items-center justify-between py-2 ">
-            <li>
-              <Link
-                className="block py-2 bg-primary-900 text-white rounded-md px-6"
-                href="/auth"
-              >
-                ورود | ثبت نام
-              </Link>
-            </li>
+            {user ? (
+              <span>{user.name}</span>
+            ) : (
+              <li>
+                <Link
+                  className="block py-2 bg-primary-900 text-white rounded-md px-6"
+                  href="/auth"
+                >
+                  ورود | ثبت نام
+                </Link>
+              </li>
+            )}
+
             <li>
               <Link className="block py-2" href="/">
                 خانه
@@ -39,7 +62,7 @@ function Header() {
             </li>
             <li>
               <Link className="block py-2" href="/cart">
-                سبد خرید
+                سبد خرید ({cart ? cart.payDetail.productIds.length : 0})
               </Link>
             </li>
           </ul>
